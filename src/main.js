b823b1b5fd546697f9ac3acd64a115dadcb9687c
Rustam <rustam391@gmail.com>
2021-05-10 19:41:28 +0300

    перевед отрисовку карточек в функцию v.2

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import LoadMoreButtonView from './view/button-show-more.js';
 import FilmPopupView from './view/popup.js';
 import FooterStatisticView from './view/footer.js';
 import { generateFilmCards } from './mock/card-for-film.js';
-import { renderTemplate, renderElement, RenderPosition, createClosePopup } from './util.js';
+import { render, RenderPosition, createClosePopup } from './util.js';
 import { CARDS_MIN_COUNT, FILMS_COUNT, FILM_COUNT_PER_STEP, FILTERS } from './const.js';
 
 const films = new Array(FILMS_COUNT).fill().map(generateFilmCards);
@@ -16,48 +16,59 @@ const siteUserElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const siteFooterElement = document.querySelector('.footer__statistics');
 
-renderElement(siteUserElement, new UserView().getElement(), RenderPosition.BEFOREEND);
-renderElement(siteMainElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND); 
-renderElement(siteMainElement, new SortView(FILTERS).getElement(), RenderPosition.BEFOREEND);
-renderElement(siteMainElement, new FilmWrapView().getElement(), RenderPosition.BEFOREEND);
+render(siteUserElement, new UserView().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND); 
+render(siteMainElement, new SortView(FILTERS).getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilmWrapView().getElement(), RenderPosition.BEFOREEND);
+
+const renderFilm = (filmListElement, film) => {
+  const filmComponent = new FilmCardView(film);
+  const filmWrapComponent = new FilmWrapView(film);
+  render(filmListElement, filmComponent.getElement(), RenderPosition.BEFOREEND);
+};
 
 const filmCardContainer = document.querySelector('.films-list__all');
 const filmCardContainerTop = document.querySelector('.films-list__top');
 const filmCardContainerMost = document.querySelector('.films-list__most');
 
-//const renderFilms = (filmView, RenderPosition) => {
+// const filmView = new FilmCardView();
+
+//const renderFilms = (container, film, RenderPosition) => {
   for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
-    const filmView = new FilmCardView(films[i]);
-    const filmComponentForPopup = filmView.getElement().querySelectorAll('.film-card__poster, .film-card__title, .film-card__comments');
-    for (let clickElement of filmComponentForPopup) {
-      clickElement.addEventListener('click', () =>{
-        renderElement(siteMainElement, new FilmPopupView(films[i]).getElement(), RenderPosition.BEFOREEND);
-      })
-    };
-    renderElement(filmCardContainer, filmView.getElement(), RenderPosition.BEFOREEND);
+    
+    // const filmComponentForPopup = filmView.getElement().querySelectorAll('.film-card__poster, .film-card__title, .film-card__comments');
+    // for (let clickElement of filmComponentForPopup) {
+    //   clickElement.addEventListener('click', () =>{
+    //     render(siteMainElement, new FilmPopupView(films[i]).getElement(), RenderPosition.BEFOREEND);
+    //   })
+    // };
+    renderFilm(filmCardContainer, films[i]);
   };
 //};
 
-const popupElement = new FilmPopupView(films[0]).getElement();
-const popupCloseElement = popupElement.querySelector('.film-details__close-btn');
+// const popupElement = new FilmPopupView(films[0]).getElement();
+// const popupCloseElement = popupElement.querySelector('.film-details__close-btn');
+// popupCloseElement.addEventListener('click', () => {
+//   console.log('sdfds');
+// })
 //createClosePopup(bodyElement, popupElement, popupCloseElement);
 
 for (let i = 0; i < CARDS_MIN_COUNT; i++) {
-  renderElement(filmCardContainerTop, new FilmCardView(films[i]).getElement(), RenderPosition.BEFOREEND);
-  renderElement(filmCardContainerMost, new FilmCardView(films[i]).getElement(), RenderPosition.BEFOREEND);
+  render(filmCardContainerTop, new FilmCardView(films[i]).getElement(), RenderPosition.BEFOREEND);
+  render(filmCardContainerMost, new FilmCardView(films[i]).getElement(), RenderPosition.BEFOREEND);
 }
 
 if (films.length > FILM_COUNT_PER_STEP) {
   let renderedFilmsCount = FILM_COUNT_PER_STEP;
   const loadMoreButtonComponent = new LoadMoreButtonView();
-  renderElement(siteMainElement, loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
+  render(siteMainElement, loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
 
   const loadMoreButton = document.querySelector('.films-list__show-more');
   loadMoreButton.addEventListener('click', (evt) => {
     evt.preventDefault();
     films
       .slice(renderedFilmsCount, renderedFilmsCount + FILM_COUNT_PER_STEP)
-      .forEach((film) => renderElement(filmCardContainer, new FilmCardView(film).getElement(), RenderPosition.BEFOREEND));  // вызваем функцию
+      .forEach((film) => render(filmCardContainer, new FilmCardView(film).getElement(), RenderPosition.BEFOREEND));  // вызваем функцию
     renderedFilmsCount += FILM_COUNT_PER_STEP;
     if (renderedFilmsCount >= films.length) {
       loadMoreButtonComponent.getElement().remove();
@@ -65,6 +76,6 @@ if (films.length > FILM_COUNT_PER_STEP) {
     }
   });
 }
-// const popupElement = new FilmPopupView(films[0]).getElement();
+
 // renderElement(siteMainElement, new FilmPopupView(films[0]).getElement(), RenderPosition.BEFOREEND);   // done
-renderElement(siteFooterElement, new FooterStatisticView().getElement(), RenderPosition.BEFOREEND);   // done
+render(siteFooterElement, new FooterStatisticView().getElement(), RenderPosition.BEFOREEND);   // done
--- src/util.js
+++ src/util.js
@@ -20,7 +20,7 @@ export const RenderPosition = {
   BEFOREEND: 'beforeend',
 };
 
-export const renderElement = (container, element, place) => {
+export const render = (container, element, place) => {
   switch (place) {
     case RenderPosition.AFTERBEGIN:
       container.prepend(element);
@@ -29,10 +29,6 @@ export const renderElement = (container, element, place) => {
       container.append(element);
       break;
   }
-};
-
-export const renderTemplate = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
 };
 
 // Принцип работы прост:
